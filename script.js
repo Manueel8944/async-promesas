51ac3c10a0ab4de132ad4f4985d017a06d153308
Manueel8944 <manueel8944@gmail.com>
2025-05-19 15:27:31 +0200

    y

---
+++ script.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function preguntar(texto) {
+  return new Promise((resolve) => {
+    rl.question(texto, (respuesta) => {
+      resolve(respuesta);
+    });
+  });
+}
+
+async function main() {
+  let nombre = await preguntar('¿Cuál es tu nombre? ');
+  let edad = await preguntar('¿Cuántos años tienes? ');
+
+  console.log(`¡Hola, ${nombre}! Tienes ${edad} años. `);
+
+  rl.close(); 
+}
+
+main();
